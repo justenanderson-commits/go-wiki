d8cf4891ce6de6f6c52d7a2186034070e0ea8557
Justen Anderson <justenanderson@gmail.com>
2023-10-05 16:17:55 -0600

    Import fmt and add main func

--- wiki.go
+++ wiki.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 )
 
@@ -14,8 +15,20 @@ func (p *Page) save() error {
 	return os.WriteFile(filename, p.Body, 0600)
 }
 
-func loadPage(title string) *Page {
+func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
-	body, _ := os.ReadFile(filename)
-	return &Page{Title: title, Body: body}
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
 }
+
+func main() {
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page, homie.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
+}
+
+// https://go.dev/doc/articles/wiki/
