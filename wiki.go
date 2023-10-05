1a8be7653791ef1cf36063fa47e9dcd9e0e6d761
Justen Anderson <justenanderson@gmail.com>
2023-10-05 15:49:15 -0600

    Add the loadPage function

--- wiki.go
+++ wiki.go
@@ -13,3 +13,9 @@ func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
 }
+
+func loadPage(title string) *Page {
+	filename := title + ".txt"
+	body, _ := os.ReadFile(filename)
+	return &Page{Title: title, Body: body}
+}
