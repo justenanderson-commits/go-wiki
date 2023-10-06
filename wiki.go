8f0695848256ea5a00c652d3a2b0b31f6c2860b0
Justen Anderson <justenanderson@gmail.com>
2023-10-06 10:38:40 -0600

    Add template global variable and refactors renderTemplate to us it

--- wiki.go
+++ wiki.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
 type Page struct {
 	Title string
 	Body  []byte
@@ -27,14 +29,10 @@ func loadPage(title string) (*Page, error) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, err := template.ParseFiles(tmpl + ".html")
+	err := templates.ExecuteTemplate(w, tmpl+"html", p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
-	}
-	err = t.Execute(w, p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
