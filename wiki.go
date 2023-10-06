d93fd776ab62001a8872f5aa22ca1b2a1267d951
Justen Anderson <justenanderson@gmail.com>
2023-10-06 10:52:20 -0600

    Add getTitle func and validPath regexp

--- wiki.go
+++ wiki.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"errors"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 )
 
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9])+$")
 
 type Page struct {
 	Title string
@@ -17,6 +20,15 @@ type Page struct {
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("invalide page title")
+	}
+	return m[2], nil
 }
 
 func loadPage(title string) (*Page, error) {
